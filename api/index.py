4409bde93a2329f14c2550293b6db8d17a5916f8
Muhamed_Ahmed <140799442+Muhammed-AhmedGithup@users.noreply.github.com>
2025-02-15 03:47:44 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,27 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask
 import utils
-from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def index():
     return 'hi'
 
-@app.route('/classify_image',methods=['GET','POST'])
-def classify_image():
-    image_data=request.form['image_data']
-    response=jsonify(utils.classify_image(image_data))
-    response.headers.add('Access-Control-Allow-Origin', '*')
 
-    return response
-
-
-
-
-
-if __name__ == '__main__':
-    utils.load_artifactes()
-    app.run()
-
-#ddddd
